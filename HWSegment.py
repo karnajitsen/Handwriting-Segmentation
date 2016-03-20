0c0332dfc31bf2d8be197075b309b2e38dcb6663
karnajitsen@gmail.com <karnajitsen@gmail.com>
2016-03-20 21:09:45 +0100

    Classified into four classes

--- HWSegment.py
+++ HWSegment.py
@@ -37,30 +37,86 @@ for i in range(1,n_clusters_):
           if temp[j] == minvalue:
               maxminarray[b[j]] = -1
 
+#Classifying the maxima and minima in 4 classes
+
+for i in range(2,n_clusters_):
       
-print('Estimated number of clusters: %d' % n_clusters_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
-      % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
-      % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, labels))
+      maxvalcur = findMaxInCluster(i,img.getEdgesFeature(0),maxminarray,labels)
+      minvalcur = findMinInCluster(i,img.getEdgesFeature(0),maxminarray,labels)
+      
+      maxvalnext = findMaxInCluster(i+1,img.getEdgesFeature(0),maxminarray,labels)
+      minvalnext = findMinInCluster(i+1,img.getEdgesFeature(0),maxminarray,labels)
+      
+      maxvalprev = findMaxInCluster(i-1,img.getEdgesFeature(0),maxminarray,labels)
+      minvalprev = findMinInCluster(i-1,img.getEdgesFeature(0),maxminarray,labels)
+      
+      diffmaxnxt = abs(maxvalnext - maxvalcur)
+      diffmaxprv = abs(maxvalprev - maxvalcur)
+      diffminnxt = abs(minvalnext - minvalcur)
+      diffminprv = abs(minvalprev - minvalcur)
+      
+      diffcurr = abs(maxvalcur - minvalcur)
+      p1 = p2 = p3 = 0
+      if diffcurr > 6 and diffcurr <= 10:
+          p1 = 1 #lower and upper baseline
+      else:
+          if (diffmaxnxt > 4 and diffmaxnxt <=10) or (diffmaxprv > 4 and diffmaxprv <=10):
+              p2 = 1 
+          if (diffminnxt > 4 and diffminnxt <=10) or (diffminprv > 4 and diffminprv <=10):
+              p3 = 1
+       
+      b = [item for item in range(len(labels)) if labels[item] == i]
+               
+      maxindex = [b[j] for j in range(len(b)) if img.getEdgesFeature(0)[b[j]][1] == maxvalcur]
+      minindex = [b[j] for j in range(len(b)) if img.getEdgesFeature(0)[b[j]][1] == minvalcur]
+           
+      if p1==1:
+          img.getEdgesFeature(0)[maxindex][2]=0
+          img.getEdgesFeature(0)[minindex][2]=1
+      elif p2==1 and p3 ==1:
+          img.getEdgesFeature(0)[maxindex][2]=2
+          img.getEdgesFeature(0)[minindex][2]=2
+      elif p2==1 and p3==0:
+          img.getEdgesFeature(0)[maxindex][2]=2
+          img.getEdgesFeature(0)[minindex][2]=1
+      elif p3==1 and p2==0:
+          img.getEdgesFeature(0)[maxindex][2]=0
+          img.getEdgesFeature(0)[minindex][2]=2
+      else: 
+          img.getEdgesFeature(0)[maxindex][2]=3
+          img.getEdgesFeature(0)[minindex][2]=3
+      
+# print('Estimated number of clusters: %d' % n_clusters_)
+# print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+# print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+# print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+# print("Adjusted Rand Index: %0.3f"
+#       % metrics.adjusted_rand_score(labels_true, labels))
+# print("Adjusted Mutual Information: %0.3f"
+#       % metrics.adjusted_mutual_info_score(labels_true, labels))
+# print("Silhouette Coefficient: %0.3f"
+#       % metrics.silhouette_score(X, labels))
 
 # Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+#n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
-def findMaxInCluster(cluster):
-    m1 = max(cluster)
-    b = [item for item in range(len(cluster)) if cluster[item] == m1]
-    return b
+def findMaxInCluster(i,image,maxminarray,labels):
+      b = [item for item in range(len(labels)) if labels[item] == i]
+      for j in range(0,len(b)):
+          if maxminarray[b[j]] == 1:
+             maxval = image[b[j]][1]
+             break
+        
+      return maxval
     
-def findMinInCluster(cluster):
-    m1 = min(cluster)
-    b = [item for item in range(len(cluster)) if cluster[item] == m1]
-    return b
+def findMinInCluster(i,image,maxminarray,labels):
+      b = [item for item in range(len(labels)) if labels[item] == i]
+      for j in range(0,len(b)):
+          if maxminarray[b[j]] == -1:
+             minval = image[b[j]][1]
+             break
+        
+      return minval
 
     
 
--- Images.py
+++ Images.py
@@ -4,10 +4,10 @@ import cv2
 
 class Images:
     def __init__(self,path):
-        self.path = path
+        self.path = path #image file path
         listing = os.listdir(path)
-        self.img = []
-        self.cnt = 0
+        self.img = [] # Data for all  n mxp images ( nxmxp )
+        self.cnt = 0 #Number of images
         for file in listing:
             self.cnt = self.cnt + 1
             fl = cv2.imread(path + file,0)
@@ -28,7 +28,7 @@ class Images:
                         if timg[i][j] <> 0:
                             feature[k] = [i,j,-1]
                             k = k + 1
-        self.imgFeatures.append(feature)
+            self.imgFeatures.append(feature)
         self.edgesFeatures = []
         for n in range(0,self.cnt-1):
             feature = np.zeros((np.count_nonzero(self.edges[n]),3))
@@ -39,7 +39,7 @@ class Images:
                         if tedge[i][j] <> 0:
                             feature[k] = [i,j,-1]
                             k = k + 1
-        self.edgesFeatures.append(feature)
+            self.edgesFeatures.append(feature)
         
      
     def applyCanny(self):
